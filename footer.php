481a950563d7331693ee6a09b45c4708ba583a3b
CogDog <cogdogblog@gmail.com>
2018-11-23 11:40:47 -0600

    footer linked

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <div class="credits section-inner">
 
 			<p><?php kidhitchcock_get_footer()?></p>
-			<p><?php _e( 'Theme by', 'hitchcock' ); ?> <a href="http://www.andersnoren.se">Anders Nor&eacute;n</a> <?php _e( '&bull; mods by', 'hamilton' ); ?> <a href="https://cog.dog">cog.dog</a></p>
+			
+			
+			<p><a href="https://github.com/cogdog/kid-hitchcock">Kid Hitchcock theme</a> by <a href="https://cog.dog/">cogdog</a> based on <a href="http://www.andersnoren.se/teman/hitchcock-wordpress-theme/">Hitchcock</a> by  <a href="http://www.andersnoren.se/">Anders Nor&eacute;n</a></p>
+
 	
 	<div class="clear"></div>
 	
